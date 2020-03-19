ce334321430b0ea21e024597c782b1d963ccb43f
peterdsharpe <peterdsharpe@gmail.com>
2020-03-19 12:14:22 -0500

    prettier plot

--- pigeon_analysis.py
+++ pigeon_analysis.py
@@ -12,48 +12,22 @@ data = pd.read_excel("data.xlsx")
 n_pigeons = np.array(data.Pigeons)
 
 # Fit a model
-distribution = stats.chi2 # this has no physical basis, just picking this dist. for kicks
+distribution = stats.norm
 params = distribution.fit(
     n_pigeons,
-    fscale = 1,
-    floc = 0,
 )
-
-# distribution = stats.ncx2
-# params = distribution.fit(
-#     n_pigeons,
-#     4,
-#     0.5,
-#     fscale = 1,
-#     floc = 0,
-# )
-
-# distribution = stats.expon
-
-# distribution = stats.norm
-# params = distribution.fit(
-#     n_pigeons,
-#     # fscale=1,
-#     # floc=0,
-# )
-
-# distribution = stats.truncnorm
-# params = distribution.fit(
-#     n_pigeons,
-#     fa = -2,
-#     fb = np.Inf,
-#     # fscale=1,
-#     floc=0,
-# )
 
 print("Fit Parameters:\n", params)
 
 # Visualize distribution
 fig, ax = plt.subplots(1, 1, figsize=(6.4, 4.8), dpi=200)
 x = np.linspace(0, np.max(n_pigeons)+0.5, 500)
+distribution_out = distribution.pdf(x, *params) / (1 - distribution.cdf(0, *params))
+x = np.hstack((0, x))
+distribution_out = np.hstack((0, distribution_out))
 plt.plot(
     x,
-    distribution.pdf(x, *params),
+    distribution_out,
     label="Model",
     color=np.array((64,143,255))/255,
     linewidth=3,
@@ -62,7 +36,7 @@ plt.plot(
 plt.fill_between(
     x,
     0,
-    distribution.pdf(x, *params),
+    distribution_out,
     color=np.array((64,143,255,255*0.2))/255,
     zorder = 10
 )
