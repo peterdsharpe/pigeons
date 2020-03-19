bd55e6f27c6f119234ee2d67d6e645400e391c60
peterdsharpe <peterdsharpe@gmail.com>
2020-03-18 23:06:35 -0500

    first (and hopefully only?) commit lol

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# pigeons
- 
+# Pigeons
+
+*by Peter Sharpe*
+
+Someone in my grad school cohort (let's call her Sydney, because her name is Sydney) recently asked me "How many pigeons do you think you could carry?". 
+
+It honestly really threw me for a loop. I thought about it for a long, long awhile and eventually came up with the answer 18 - since I think I could stuff quite a few pigeons in a trench coat (might be painful, but I could make it a block or so?)?
+
+But I wanted to see what other people thought they could manage, so I crowdsourced data from the most reliable data collection website I could think of: Tinder. (The best part was that this was totally free - I just had to mortgage my dating life for a few weeks!)
+
+Shoutout goes to our outlier, Yasmeen from Harvard, who simply replied "How many fit on a skewer?" (Some back-of-the-napkin math on average skewer length converted this to around 10 pigeons.) You live your best life, Yasmeen.
+
+![dataviz](pigeons.svg)
---
+++ pigeon_analysis.py
@@ -0,0 +1,62 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.style as style
+import seaborn as sns
+
+sns.set(font_scale=1)
+import pandas as pd
+from scipy import stats
+
+# Fetch data
+data = pd.read_excel("data.xlsx")
+n_pigeons = np.array(data.Pigeons)
+
+# Fit a model
+# distribution = stats.chi2
+# distribution = stats.gamma
+# distribution = stats.expon
+# distribution = stats.norm
+distribution = stats.truncnorm
+
+params = distribution.fit(
+    n_pigeons,
+    0,
+    fb = np.Inf,
+    # fscale=1,
+    # floc=0,
+)
+
+print("Fit Parameters:\n", params)
+
+# Visualize distribution
+fig, ax = plt.subplots(1, 1, figsize=(6.4, 4.8), dpi=200)
+x = np.linspace(0, np.max(n_pigeons)+0.5, 500)
+plt.plot(
+    x,
+    distribution.pdf(x, *params),
+    label="Model",
+    color=np.array((64,143,255))/255,
+    linewidth=3,
+)
+plt.hist(
+    n_pigeons,
+    bins=np.arange(np.ceil(np.max(n_pigeons)) + 2) - 0.5,
+    density=True,
+    label="Data",
+    color=np.array((255,64,99))/255
+)
+# plt.text(6,0.20,"Note: All responses rounded\nto the nearest whole pigeon.", fontsize = 10)
+plt.annotate(
+    "Note: All responses rounded\nto the nearest whole pigeon.",
+    (0.45, 0.9),
+    xycoords="axes fraction",
+    fontsize = 10
+)
+plt.xlabel("Number of Pigeons They Could Carry [pigeons]")
+plt.ylabel("Probability Distribution Function [-]")
+plt.title("A Survey of Tinder:\n\"How many pigeons do you think you could carry?\"")
+plt.tight_layout()
+plt.legend()
+plt.savefig("pigeons.svg")
+plt.savefig("pigeons.png")
+plt.show()
